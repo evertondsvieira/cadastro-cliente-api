6325dc61dd752517c2fe611f454894e4a6ac47ee
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-08 00:14:51 -0300

    Feat: added views

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Client
+from .serializers import ClientSerializer
 
-# Create your views here.
+class ClientView(generics.CreateAPIView):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+
+
+class ClientDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
