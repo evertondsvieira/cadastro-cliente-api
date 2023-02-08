3af2982ffbf1e5da9b7bddde41cef26bcbc37d46
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-08 14:10:33 -0300

    Fix: remove migrations

--- clients/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Client(models.Model):
-    full_name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.EmailField(max_length=127, unique=True)
-    phone = models.IntegerField()
