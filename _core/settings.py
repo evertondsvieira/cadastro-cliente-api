2b3116c67910652dfde09b75ffd93795780f24e6
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-11 18:05:56 -0300

    Deploy config

--- _core/settings.py
+++ _core/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "USER": os.getenv('POSTGRES_USERNAME'),
         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
         "HOST": "127.0.0.1",
-        "PORT": "6378",
+        "PORT": "5432",
     },
 
     "sqlite3": {
