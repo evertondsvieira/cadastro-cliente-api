d588609058cfef53e87ffd4ca5a4ac5d69319341
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-11 11:14:00 -0300

    Fix: fixed model

--- users/models.py
+++ users/models.py
@@ -1,11 +1,8 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     full_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=127, unique=True)
     phone = models.IntegerField()
-
-def __str__(self):
-    return self.username
+    created_at = models.DateTimeField(auto_now_add=True)
